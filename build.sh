b7301b0df4393ccffe6faf6aae58eb23ce375484
ChrisEke <45903979+ChrisEke@users.noreply.github.com>
2021-01-12 01:13:24 +0100

    Update build script

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set -e 
+
 talkyard_version_url="https://raw.githubusercontent.com/debiki/talkyard-versions/master/version-tags.log"
 talkyard_version_output="lib/talkyard/talkyard-version.libsonnet"
 manifest_dir="manifests"
@@ -6,10 +8,11 @@ kustomize_base="kustomization.yaml"
 kustomize_manifests="$manifest_dir/$kustomize_base"
 
 # Get latest Talkyard version
-latest_version=$(curl -S --silent $talkyard_version_url | grep -v -e "WIP\|^$" | tail -1)
+latest_version=$(curl -S --silent --fail $talkyard_version_url | grep -v -e "WIP\|^$" | tail -1; test ${PIPESTATUS[0]} -eq 0)
 echo "{ _version+:: { talkyard+:: { version: '"$latest_version"' } } }" > $talkyard_version_output
 
 ## Start clean by removing old manifests
+mkdir -p $manifest_dir
 rm -r $manifest_dir/
 
 ## Leverage tanka export function to generate yaml manifests
